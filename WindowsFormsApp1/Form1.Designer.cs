608ae18df474ae35278b2d8008f57a4cc83f0646
nasalina120 <153774834+nasalina120@users.noreply.github.com>
2024-10-02 17:19:43 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(138, 193);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(237, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 186);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Decide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int limit = Convert.ToInt32(textBox1.Text);
+            string text = richTextBox1.Text;
+
+            // Split the text into lines
+            string[] lines = text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Process each line
+            foreach (string line in lines)
+            {
+                // Split the line by spaces
+                string[] words = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            }
+            string lastLine = "";
+            string temp = "";
+            if (limit < lines[0].Length)
+            {
+
+                if (limit > 2)
+                {
+                    //////////////////////////////////////////////FIRST
+                    string first = Convert.ToString(lines[0][0]);
+                    for (int i = 1; i < limit - 2; i++)
+                    {
+                        first += Convert.ToString(lines[0][i]);
+                    }
+                    first += "..." + lines[0][lines[0].Length - 1];
+                    listBox1.Items.Add(first);
+                    lastLine = first;
+                    //////////////////////////////////////////////FIRST
+                }
+                if (limit == 2)
+                {
+                    string first = Convert.ToString(lines[0][0]);
+                    first += "...";
+                    listBox1.Items.Add(first);
+                    lastLine = first;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Limit must be lesser than the length of the word", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+
+            void Shorten(int imp)
+            {
+                if (limit < lines[imp].Length)
+                {
+                    if (limit > 3)
+                    {
+                        int unqNum = 0;
+                        string temp1 = Convert.ToString(lines[imp][0]);
+                        for (int i = 1; i < lines[imp].Length - 1; i++)
+                        {
+                            if (lines[imp][i] != lines[imp - 1][i])
+                            {
+                                if (i > 1)
+                                {
+                                    temp1 = temp1 + "...";
+                                }
+                                temp1 += Convert.ToString(lines[imp][i]);
+
+                                unqNum = i;
+                                break;
+                            }
+                        }
+                        if (unqNum != 0)
+                        {
+                            if (unqNum + (limit - 4) < lines[imp].Length - 1)
+                            {
+                                if (unqNum > 1)
+                                {
+                                    for (int i = unqNum; i < unqNum + (limit - 5); i++)
+                                    {
+                                        temp1 += lines[imp][i + 1];
+                                    }
+                                }
+                                else
+                                {
+                                    for (int i = unqNum; i < unqNum + (limit - 4); i++)
+                                    {
+                                        temp1 += lines[imp][i + 1];
+                                    }
+                                }
+                            }
+                            if (unqNum + (limit - 4) > lines[imp].Length - 1)
+                            {
+                                if (unqNum > 1)
+                                {
+                                    for (int i = unqNum; i > unqNum - (limit - 5); i--)
+                                    {
+                                        temp1 += lines[imp][i - 1];
+                                    }
+                                    string original = temp1;
+                                    int startIndex = 4;
+                                    int endIndex = temp1.Length - 1;
+
+                                    // Extract the part to be inverted
+                                    string partToInvert = original.Substring(startIndex, endIndex - startIndex + 1);
+
+                                    // Invert the extracted part
+                                    char[] charArray = partToInvert.ToCharArray();
+                                    Array.Reverse(charArray);
+                                    string invertedPart = new string(charArray);
+
+                                    // Reconstruct the final string
+                                    string result = original.Substring(0, startIndex) + invertedPart + original.Substring(endIndex + 1);
+                                    temp1 = result;
+                                }
+
+
+                            }
+
+                            temp1 += "...";
+                            temp1 += Convert.ToString(lines[imp][lines[imp].Length - 1]);
+                            listBox1.Items.Add(temp1);
+                            lastLine = temp1;
+                        }
+                        if (unqNum == 0)
+                        {
+                            listBox1.Items.Add(lastLine + " #1");
+                        }
+                    }
+                    if (limit == 3)
+                    {
+                        int unqNum = 0;
+                        string temp1 = Convert.ToString(lines[imp][0]);
+                        for (int i = 1; i < lines[imp].Length; i++)
+                        {
+                            if (lines[imp][i] != lines[imp - 1][i])
+                            {
+                                unqNum = i;
+                                temp1 += Convert.ToString(lines[imp][i]);
+                                temp1 += "...";
+                                listBox1.Items.Add(temp1);
+                                lastLine = temp1;
+                                break;
+                            }
+
+                        }
+                        if (unqNum == 0)
+                        {
+                            listBox1.Items.Add(lastLine + " #1");
+                        }
+                    }
+                    if (limit == 2)
+                    {
+                        int unqNum = 0;
+                        string temp1;
+                        for (int i = 1; i < lines[imp].Length; i++)
+                        {
+                            if (lines[imp][i] != lines[imp - 1][i])
+                            {
+                                temp1 = Convert.ToString(lines[imp][i]);
+                                unqNum = i;
+                                temp1 += "...";
+                                listBox1.Items.Add(temp1);
+                                lastLine = temp1;
+                                break;
+                            }
+
+                        }
+                        if (unqNum == 0)
+                        {
+                            listBox1.Items.Add(lastLine + " #1");
+                        }
+
+
+                    }
+
+                }
+
+
+            }
+            for (int i = 1; i < lines.Length; i++)
+            {
+                Shorten(i);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            listBox1.Items.Clear();
+        }
+    }
+}
+    
+
